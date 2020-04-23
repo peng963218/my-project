a38f09eb04a82816bccc0bada657a00cf434ae65
unknown <pengdaiqing@credit-zonda.com>
2020-04-23 12:04:33 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import './style/index.scss'
+// import SlideVerify from 'vue-monoplasty-slide-verify'
+
+// Vue.use(SlideVerify)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app")
---
+++ src/views/About.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="About">
+<!--    <slide-verify-->
+<!--      :l="42"-->
+<!--      :r="10"-->
+<!--      :w="310"-->
+<!--      :h="155"-->
+<!--      @success="onSuccess"-->
+<!--      @fail="onFail"-->
+<!--      @refresh="onRefresh"-->
+<!--      :slider-text="text"-->
+<!--    ></slide-verify>-->
+<!--    <div>{{msg}}</div>-->
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'About',
+    data() {
+      return {
+        msg: '',
+        text: '向右滑动'
+      }
+    },
+    created() {
+    },
+    methods: {
+      onSuccess() {
+        this.msg = 'login success'
+      },
+      onFail() {
+        this.msg = ''
+      },
+      onRefresh() {
+        this.msg = ''
+      }
+    },
+    components: {},
+    computed: {},
+    beforeDestroy() {
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .About {
+  }
+</style>
